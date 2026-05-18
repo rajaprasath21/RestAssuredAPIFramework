cea7cddea1685bfc016181e8d581d47a7f889f8f
rajaprasath21 <rajaprasath21@gmail.com>
2026-05-18 12:51:15 +0530

    Update profile Test changes & testng.xml folder structure changed

--- src/test/java/com/api/tests/UpdateProfileTest.java
+++ src/test/java/com/api/tests/UpdateProfileTest.java
@@ -3,6 +3,7 @@ package com.api.tests;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.api.base.AuthService;
 import com.api.base.UserProfileManagementService;
@@ -10,6 +11,7 @@ import com.api.models.request.LoginRequest;
 import com.api.models.request.ProfileRequest;
 import com.api.models.response.LoginResponse;
 import com.api.models.response.UserProfileResponse;
+import com.api.utilities.ExtentManager;
 
 import io.restassured.response.Response;
 
@@ -17,19 +19,37 @@ import io.restassured.response.Response;
 public class UpdateProfileTest {
 	@Test
 	public void updateProfileTest() {
+		SoftAssert softAssert=new SoftAssert();
+		
 		AuthService authService=new AuthService();
 		Response response = authService.login(new LoginRequest("rajaprasath","welcome"));
+		ExtentManager.logStep("Login Resquest sent");
+
 		LoginResponse loginResponse = response.as(LoginResponse.class);
+		if(loginResponse.getToken()!=null) {
+			ExtentManager.logStep("API Loginned!");
+		}
 		System.out.println(response.asPrettyString());
 		System.out.println("----------------------------------");
 		
 		UserProfileManagementService userProfileManagementService=new UserProfileManagementService();
+		
 		Response response2 = userProfileManagementService.getProfile(loginResponse.getToken());
 		System.out.println(response2.asPrettyString());
 		
 		UserProfileResponse userProfileResponse= response2.as(UserProfileResponse.class);
 		System.out.println(userProfileResponse.getUsername());
-		Assert.assertEquals(userProfileResponse.getUsername(), "rajaprasath");
+		ExtentManager.logStep("Loginned User : "+userProfileResponse.getUsername());
+		
+		if (userProfileResponse.getUsername().equals("rajaprasath")) {
+			ExtentManager.logStepValidationForAPI("Username Validation Passed!");
+			softAssert.assertTrue(true);
+		}else {
+			ExtentManager.logFailureAPI("Username Validation Failed!");
+			softAssert.assertTrue(false);
+		}
+		
+		//softAssert.assertEquals(userProfileResponse.getUsername(), "rajaprasath");
 		
 		System.out.println("----------------------------------");
 		
@@ -42,6 +62,16 @@ public class UpdateProfileTest {
 
 		Response response3 = userProfileManagementService.updateProfile(loginResponse.getToken(), profileRequest);
 		System.out.println(response3.asPrettyString());
+		System.out.println(response3.getStatusCode());
+		
+		
+		if (response3.getStatusCode()==200) {
+			ExtentManager.logStepValidationForAPI("The Profile updated Successfully!");
+		}else {
+			ExtentManager.logFailureAPI("The Profile updation got failed!");
+		}
+		
+		softAssert.assertAll();
 		
 	}
 
